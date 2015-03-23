aa1c2af07843b16e5f9b560439f29fd5c0189328
Jason Walsh <j@wal.sh>
2015-03-23 12:49:12 -0400

    Porting dotfile keybindings

--- config/bindings.el
+++ config/bindings.el
@@ -4,4 +4,29 @@
 (define-key global-map (kbd "C-+") 'text-scale-increase)
 (define-key global-map (kbd "C--") 'text-scale-decrease)
 
+;; This should already be part of
+;; http://eschulte.me/emacs-starter-kit/starter-kit-bindings.html
 (global-set-key "\C-x\C-g" 'magit-status)
+
+;; ;; https://github.com/briancarper/dotfiles/blob/master/.emacs
+;; (global-set-key "\C-cl" 'org-store-link)
+;; (global-set-key "\C-cc" 'org-capture)
+;; (global-set-key "\C-ca" 'org-agenda)
+;; (global-set-key "\C-cb" 'org-iswitchb)
+
+;; ;; Undo
+;; (require 'undo-tree)
+;; (global-undo-tree-mode)
+
+;; (global-set-key "\C-R" 'undo-tree-undo)
+;; (global-set-key "\C-\\;" 'comment-or-uncomment-region)
+;; (global-set-key [C-tab] 'other-window)
+
+;; ;; Yegge
+;; (global-set-key "\C-x\C-m" 'execute-extended-command)
+;; (global-set-key "\C-c\C-m" 'execute-extended-command)
+;; (global-set-key "\C-w" 'backward-kill-word)
+;; (global-set-key "\C-x\C-k" 'kill-region)
+;; (global-set-key "\C-c\C-k" 'kill-region)
+;; (defalias 'qrr 'query-replace-regexp)
+;; (global-set-key [f5] 'call-last-kbd-macro)
--- init.el
+++ init.el
@@ -3,5 +3,13 @@
 ;; Use this file to initiate the pack configuration.
 ;; See README for more information.
 
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+
+(defun load-.emacs ()
+  "Reload .emacs"
+  (interactive)
+  (load "~/.live-packs/jwalsh-pack/init.el"))
+
 ;; Load bindings config
 (live-load-config-file "bindings.el")
